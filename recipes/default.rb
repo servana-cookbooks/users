747b6262aeabd95df435a45f65aaaddec36da33a
Tass Skoudros <tass@skystack.com>
2013-05-03 13:39:29 +0100

    update to check the variables before looping
    Signed-off-by: Tass Skoudros <tass@skystack.com>

--- recipes/default.rb
+++ recipes/default.rb
@@ -12,85 +12,90 @@ group "sys-admin" do
   system false
 end 
 
-node["groups"].each do |g|
+if node["groups"]
+  node["groups"].each do |g|
+  	group g['name'] do
+  	  action :create
+  	  system g['is_system']
+  	end 
 
-	group g['name'] do
-	  action :create
-	  system g['is_system']
-	end 
-
-	if g['grant_sudo']
-  	node.set['authorization']['sudo']['groups'] << g
+  	if g['grant_sudo']
+    	node.set['authorization']['sudo']['groups'] << g
+    end
   end
-
 end
 
-node["users"].each do |u|
-  
-  if u['home']
-  	home_dir = "/home/#{u['username']}"
-  elsif u['home_dir']
-	home_dir = "#{u['home_dir']}"
+if node["users"]
+
+  node["users"].each do |u|
+    
+    if u['home']
+    	home_dir = "/home/#{u['username']}"
+    elsif u['home_dir']
+  	home_dir = "#{u['home_dir']}"
+    end
+
+    if home_dir
+    	manage_home = true
+    else 
+    	manage_home = false
+    end
+
+    if u['shell']
+    	user_shell = u['shell']
+    else
+    	user_shell = node['user']['default']['shell']
+    end
+
+    user u['username'] do
+      shell user_shell
+      comment u['comment']
+      supports :manage_home => manage_home
+      home home_dir
+    end
+
+    group "#{u['username']}" do
+    	members "#{u['username']}"
+    	append true
+    end
+
+    directory "#{home_dir}/.ssh" do
+      owner u['username']
+      group u['username']
+      mode "0700"
+    end
+
+
+    if u['ssh_keys']
+
+      template "#{home_dir}/.ssh/authorized_keys" do
+       source "authorised_keys.erb"
+       owner u['username']
+       group u['username']
+       mode "0600"
+       variables :ssh_keys => u['ssh_keys']
+     end
+
+    end
+
+    if u['is_admin']
+    	group "sys-admin" do
+    		action :modify
+    		members ["#{u['username']}"]
+    		append true
+  	end 
+    end
+
+    if u['grant_sudo']
+    	node.set['authorization']['sudo']['users'] << u
+    end
+
+    execute "passwd -l #{u['username']}"
+
+    	#default['authorization']['sudo']['groups'] = Array.new 
+  	  #default['authorization']['sudo']['users'] = Array.new
+
   end
-
-  if home_dir
-  	manage_home = true
-  else 
-  	manage_home = false
-  end
-
-  if u['shell']
-  	user_shell = u['shell']
-  else
-  	user_shell = node['user']['default']['shell']
-  end
-
-  user u['username'] do
-    shell user_shell
-    comment u['comment']
-    supports :manage_home => manage_home
-    home home_dir
-  end
-
-  group "#{u['username']}" do
-  	members "#{u['username']}"
-  	append true
-  end
-
-  directory "#{home_dir}/.ssh" do
-    owner u['username']
-    group u['username']
-    mode "0700"
-  end
-
-  if u['ssh_keys']
-
-    template "#{home_dir}/.ssh/authorized_keys" do
-     source "authorised_keys.erb"
-     owner u['username']
-     group u['username']
-     mode "0600"
-     variables :ssh_keys => u['ssh_keys']
-   end
-
-  end
-
-  if u['is_admin']
-  	group "sys-admin" do
-  		action :modify
-  		members ["#{u['username']}"]
-  		append true
-	end 
-  end
-
-  if u['grant_sudo']
-  	node.set['authorization']['sudo']['users'] << u
-  end
-
-  execute "passwd -l #{u['username']}"
-
-  	#default['authorization']['sudo']['groups'] = Array.new 
-	  #default['authorization']['sudo']['users'] = Array.new
 
 end
 
